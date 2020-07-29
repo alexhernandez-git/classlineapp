d1ff2fa945096f6ce744a5670dfdfd30c3fef8c8
Alex Hernandez <vlexhndz@gmail.com>
2020-07-29 19:46:26 +0200

    fewefwa

--- screens/Academy/MainPageAcademyScreen.tsx
+++ screens/Academy/MainPageAcademyScreen.tsx
@@ -9,8 +9,13 @@ import {
   Dimensions,
 } from "react-native";
 import { RootStackParamList } from "../../types";
-import { TouchableOpacity } from "react-native-gesture-handler";
-const Item = () => (
+import { YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings([
+  "VirtualizedLists should never be nested", // TODO: Remove when fixed
+]);
+import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
+const Video = () => (
   <TouchableOpacity style={styles.academyContainer}>
     <View>
       <Image
@@ -18,14 +23,40 @@ const Item = () => (
         source={require("../../assets/images/no-foto.png")}
       />
       <View style={styles.info}>
-        <Image
-          style={styles.imageAvatar}
-          source={require("../../assets/images/avatar.png")}
-        />
         <View style={styles.infoText}>
           <Text style={styles.title}>MainNavigator</Text>
-          <Text style={styles.subtitle}>Classline Academy</Text>
+          <Text style={styles.subtitle}>21/08/2019</Text>
         </View>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+const Playlist = () => (
+  <TouchableOpacity style={styles.academyContainer}>
+    <View>
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/no-foto.png")}
+      />
+      <View style={styles.info}>
+        <View style={styles.infoText}>
+          <Text style={styles.title}>MainNavigator</Text>
+          <Text style={styles.subtitle}>14 Videos</Text>
+        </View>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+const Podcast = () => (
+  <TouchableOpacity style={styles.podcastContainer}>
+    <Image
+      style={styles.imagePodcast}
+      source={require("../../assets/images/no-foto.png")}
+    />
+    <View style={styles.info}>
+      <View style={styles.infoText}>
+        <Text style={styles.title}>MainNavigator</Text>
+        <Text style={styles.subtitle}>14 Videos</Text>
       </View>
     </View>
   </TouchableOpacity>
@@ -59,28 +90,65 @@ export default function MainPageAcademyScreen({
       title: "Six Academy",
     },
   ];
-  const renderItem = ({ item, separators }: any) => <Item />;
+  const renderVideo = ({ item, separators }: any) => <Video />;
+  const renderPlaylist = ({ item, separators }: any) => <Playlist />;
+  const renderPodcast = ({ item, separators }: any) => <Podcast />;
   const flatListItemSeparator = () => {
     return <View style={styles.separator} />;
   };
+  const flatListHorizontalItemSeparator = () => {
+    return <View style={styles.separatorHorizontal} />;
+  };
   return (
-    <FlatList
-      ItemSeparatorComponent={flatListItemSeparator}
-      data={DATA}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-    />
+    <View>
+      <ScrollView nestedScrollEnabled>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Videos populares</Text>
+          <FlatList
+            horizontal={true}
+            ItemSeparatorComponent={flatListHorizontalItemSeparator}
+            data={DATA}
+            renderItem={renderVideo}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>
+            Listas de reproducion populares
+          </Text>
+          <FlatList
+            horizontal={true}
+            ItemSeparatorComponent={flatListHorizontalItemSeparator}
+            data={DATA}
+            renderItem={renderPlaylist}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Podcasts populares</Text>
+          <FlatList
+            ItemSeparatorComponent={flatListItemSeparator}
+            data={DATA}
+            renderItem={renderPodcast}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   academyContainer: {
-    width: Dimensions.get("window").width,
+    width: Dimensions.get("window").width - 100,
   },
   image: {
-    flex: 1,
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").width / 2,
+    width: Dimensions.get("window").width - 100,
+    height: Dimensions.get("window").width / 2 - 40,
+  },
+  imagePodcast: {
+    width: 100,
+    height: 100,
   },
   info: {
     flexDirection: "row",
@@ -93,17 +161,26 @@ const styles = StyleSheet.create({
   },
   infoText: {
     padding: 2,
-    paddingHorizontal: 10,
     justifyContent: "space-between",
+  },
+  sectionTitle: {
+    fontWeight: "bold",
+    fontSize: 16,
+    margin: 10,
   },
   title: {
     fontWeight: "bold",
     fontSize: 16,
   },
   subtitle: { fontSize: 12 },
+  separatorHorizontal: { height: "100%", width: 20 },
   separator: {
     borderBottomWidth: 0.5,
-    marginBottom: 20,
+    marginVertical: 10,
     borderBottomColor: "#ccc",
+  },
+  section: {},
+  podcastContainer: {
+    flexDirection: "row",
   },
 });
