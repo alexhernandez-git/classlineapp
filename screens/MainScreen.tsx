4cafa5c25e90af702be8bdf34b8cec13a0bf29e4
Alex Hernandez <vlexhndz@gmail.com>
2020-07-31 00:01:01 +0200

    weafe

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
   textInput: {
     height: 40,
     width: 250,
-    borderColor: "gray",
+    borderColor: "#ccc",
     borderWidth: 0.5,
     padding: 10,
     borderRadius: 6,
